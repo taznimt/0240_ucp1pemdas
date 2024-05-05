c9411f9eda778981c95fd6119c9a720dd30ab837
taznimt <taznimanwar06@gmail.com>
2024-05-05 13:14:16 +0700

    Add project files.

---
+++ 0240_ucp1pemdas/0240_ucp1pemdas.cpp
@@ -0,0 +1,85 @@
+//1.a.string nama; b. int i; c.int bilangan1;
+//2.prosedur = tipe data yang tidak menyimpan nilai contohnya int bilangan; sedangkan fungsi = tipe data yang menyimpan nilai contohny int arr[20];
+
+//3.contoh looping =
+//int main()
+//{
+	//string pilihan;
+	//do
+	//{
+	//cout << "makan" << endl;
+		//cout << "main game" << endl;
+		//cout << "Ibadah" << endl;
+
+		//cout << "Apakah anda masih hidup? (Yes/No)";
+		//cin >> pilihan;
+
+	//} while (pilihan == "Yes");
+
+//}
+
+//4.contoh cinditional statement=
+// int main() {
+//int bilangan;
+
+//srand(time(0));
+
+//bilangan = rand() % 10;
+
+//while (bilangan % 2)
+//{
+	//cout << "Bilangan" << bilangan << "adalah Ganjil" << endl;
+	//bilangan = rand() % 10;
+//}
+//cout << "Bilangan " << bilangan << "adalah Genap" << endl;
+//}
+// 
+//5.
+// int main(){
+//Mahasiswa mhs;
+//cout << "Nomor Mahasiswa : ";
+//cin >> mhs.nim;
+//cout << "Nama Mahasiswa : ";
+//cout << "Alamat Mahasiswa : ";
+//cin >> mhs.alamat;
+//cout << "Umur Mahasiswa : ";
+//cin >> mhs.umur;
+//cout << endl;
+//cout << "\n NIM : " << mhs.nim;
+//cout << "\n Nama : " << mhs.nama;
+//cout << "\n Alamat : " << mhs.alamat;
+//cout << "\n Umur : " << mhs.umur;
+//}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int lokasi1(55);
+	int lokasi2;
+	int lokasi3;
+
+	string NamaKota;
+	cout << "Masukan lokasi1 : " << endl;
+	cin >> lokasi1;
+	cout << "Masukan lokasi2 : " << endl;
+	cin >> lokasi2;
+	cout << "Masukanlokasi3 : " << endl;
+	cin >> lokasi3;
+	if (Lokasi > 75)
+	{
+		NamaKota = Tanggerang//tidak sehat
+	}
+	else if
+	{
+		NamaKota = Bekasi//sehat
+	}
+	else(sensor(0) = 45)
+	{
+		NamaKota = Tanggerang //tidak sehat
+	}
+	cout << "kualitas udara sehat = " << sehat << endl;
+	cout << " kualitas udara tidak sehat<< tidak sehat << endl;
+		cout << "sensor = 45 " << sensor << endl;
+}
